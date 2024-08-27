9c4605115aee1ac1d61258a01be59e0cb65c7ef4
Akash290104 <mishra.ak801@gmail.com>
2024-08-27 11:18:43 +0530

    updated

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.jsx
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.jsx
@@ -1,40 +1,32 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import styles from "../../styling/UpdateGroupChatModal.module.scss";
 import { ChatState } from "../../context/chatProvider";
 import { ChatLoading } from "./SearchBar";
 import axios from "axios";
 import { debounce } from "../../config/debounce";
 
-const UserBadge = ({ user, handleFunction }) => {
-  return (
-    <div className={styles.badgecontainer}>
-      <div className={styles.badgecontent}>
-        <div className={styles.badgename}>{user.name}</div>
-        <div className={styles.badgeclose}>
-          <button onClick={handleFunction}>X</button>
-        </div>
+const UserBadge = ({ user, handleFunction }) => (
+  <div className={styles.badgecontainer}>
+    <div className={styles.badgecontent}>
+      <div className={styles.badgename}>{user.name}</div>
+      <div className={styles.badgeclose}>
+        <button onClick={handleFunction}>X</button>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-const User = ({ user, handleFunction }) => {
-  return (
-    <div className={styles.customUser} onClick={handleFunction}>
-      <div className={styles.pic}>
-        <img src={user.pic} alt="User" />
-      </div>
-      <div className={styles.details}>
-        <div className={styles.name1} id="">
-          {user.name}
-        </div>
-        <div className={styles.mail} id="">
-          {user.email}
-        </div>
-      </div>
+const User = ({ user, handleFunction }) => (
+  <div className={styles.customUser} onClick={handleFunction}>
+    <div className={styles.pic}>
+      <img src={user.pic} alt="User" />
     </div>
-  );
-};
+    <div className={styles.details}>
+      <div className={styles.name1}>{user.name}</div>
+      <div className={styles.mail}>{user.email}</div>
+    </div>
+  </div>
+);
 
 const UpdateGroupChatModal = ({
   hideUpdateModal,
@@ -43,59 +35,48 @@ const UpdateGroupChatModal = ({
   fetchMessages,
 }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
-
-  const [groupchatName, setGroupChatName] = useState();
-  const [search, setSearch] = useState();
+  const [groupchatName, setGroupChatName] = useState("");
+  const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [renameLoading, setRenameLoading] = useState(false);
   const [noUsers, setNoUsers] = useState(false);
-  const [showUserBadge, setShowUserBadge] = useState(false);
 
-  const closeFunction = () => {
-    setShowUserBadge(false);
-  };
-
-  const handleSearch = useCallback(
-    debounce(async (search) => {
-      if (!search) {
-        setSearchResult([]);
-        setNoUsers(false);
-        return;
-      }
-      try {
-        setLoading(true);
-        const config = {
-          headers: {
-            Authorization: `Bearer ${user.data.token}`,
-          },
-        };
-
-        const response = await axios.get(
-          `https://chat-app-3-jpcn.onrender.com/api/user?search=${search}`,
-          config
-        );
-
-        setLoading(false);
-        setNoUsers(true);
-        setSearchResult(response.data.users);
-      } catch (error) {
-        setLoading(false);
-        alert("Error finding searched users");
-        console.log(error);
-      }
-    }, 500),
-    [user.data.token]
-  );
+  const handleSearch = debounce(async (searchTerm) => {
+    if (!searchTerm) {
+      setSearchResult([]);
+      setNoUsers(false);
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.data.token}`,
+        },
+      };
+      const response = await axios.get(
+        `https://chat-app-3-jpcn.onrender.com/api/user?search=${searchTerm}`,
+        config
+      );
+      setLoading(false);
+      setNoUsers(response.data.users.length === 0);
+      setSearchResult(response.data.users);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error finding searched users", error);
+      alert("Error finding searched users");
+    }
+  }, 500);
 
   const handleAddUser = async (selectedUser) => {
-    if (selectedChat.users.find((a) => a._id === selectedUser._id)) {
+    if (selectedChat.users.find((u) => u._id === selectedUser._id)) {
       alert("Selected user is already a member of the group");
       return;
     }
 
     if (selectedChat.groupAdmin._id !== user.data.existingUser._id) {
-      alert("Unauthorized access \nOnly admin can add or remove members");
+      alert("Unauthorized access. Only admin can add or remove members");
       return;
     }
 
@@ -106,19 +87,18 @@ const UpdateGroupChatModal = ({
           Authorization: `Bearer ${user.data.token}`,
         },
       };
-
       const response = await axios.put(
         "https://chat-app-3-jpcn.onrender.com/api/chat/addtogroup",
         { chatId: selectedChat._id, userId: selectedUser._id },
         config
       );
-
       setSelectedChat(response.data.added);
-      setFetchAgain(!fetchAgain);
+      setFetchAgain((prev) => !prev);
       alert("New user added");
-      setLoading(false);
     } catch (error) {
+      console.error("Error adding the selected user to the group", error);
       alert("Error adding the selected user to the group");
+    } finally {
       setLoading(false);
     }
   };
@@ -128,7 +108,7 @@ const UpdateGroupChatModal = ({
       selectedChat.groupAdmin._id !== user.data.existingUser._id &&
       selectedUser._id !== user.data.existingUser._id
     ) {
-      alert("Unauthorized access \nOnly admin can add or other remove members");
+      alert("Unauthorized access. Only admin can remove members.");
       return;
     }
 
@@ -139,7 +119,6 @@ const UpdateGroupChatModal = ({
           Authorization: `Bearer ${user.data.token}`,
         },
       };
-
       const response = await axios.put(
         "https://chat-app-3-jpcn.onrender.com/api/chat/removefromgroup",
         { chatId: selectedChat._id, userId: selectedUser._id },
@@ -149,26 +128,24 @@ const UpdateGroupChatModal = ({
       if (selectedUser._id === user.data.existingUser._id) {
         alert(`You are leaving the group ${selectedChat.chatName}`);
         setSelectedChat();
-        setFetchAgain(!fetchAgain);
-        setLoading(false);
-        fetchMessages();
         hideUpdateModal();
       } else {
         setSelectedChat(response.data.removed);
         alert("User removed from the group");
-        setFetchAgain(!fetchAgain);
-        setLoading(false);
-        fetchMessages();
       }
+      setFetchAgain((prev) => !prev);
+      fetchMessages();
     } catch (error) {
+      console.error("Error removing the selected user from the group", error);
       alert("Error removing the selected user from the group");
+    } finally {
       setLoading(false);
     }
   };
 
   const updateName = async () => {
     if (!groupchatName) {
-      alert("Use a name for gorup chat");
+      alert("Please enter a group chat name");
       return;
     }
 
@@ -180,32 +157,27 @@ const UpdateGroupChatModal = ({
           Authorization: `Bearer ${user.data.token}`,
         },
       };
-
       const response = await axios.put(
         "https://chat-app-3-jpcn.onrender.com/api/chat/rename",
         { id: selectedChat._id, name: groupchatName },
         config
       );
 
-      console.log("Updated group chat is ", response);
-
       setSelectedChat(response.data.updatedChat);
-      setRenameLoading(false);
+      alert("Group chat name updated");
     } catch (error) {
-      console.log("Error updating group chat name", error);
-      setRenameLoading(false);
+      console.error("Error updating group chat name", error);
       alert("Error updating group chat name");
+    } finally {
+      setRenameLoading(false);
     }
   };
 
-  const handleInputChange = useCallback(
-    (e) => {
-      const value = e.target.value;
-      setSearch(value);
-      handleSearch(value);
-    },
-    [handleSearch]
-  );
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setSearch(value);
+    handleSearch(value);
+  };
 
   return (
     <div className={styles.backdrop}>
@@ -224,8 +196,8 @@ const UpdateGroupChatModal = ({
         <div className={styles.users}>
           {selectedChat.users.map((user) => (
             <UserBadge
-              user={user}
               key={user._id}
+              user={user}
               handleFunction={() => handleRemoveUser(user)}
             />
           ))}
@@ -235,27 +207,27 @@ const UpdateGroupChatModal = ({
           <div className={styles.nameField}>
             <input
               type="text"
+              value={groupchatName}
               onChange={(e) => setGroupChatName(e.target.value)}
               placeholder="Enter Group name"
             />
           </div>
           <div className={styles.updateBtn}>
-            <button onClick={() => updateName()}>Update</button>
+            <button onClick={updateName} disabled={renameLoading}>
+              {renameLoading ? "Updating..." : "Update"}
+            </button>
           </div>
-          {renameLoading ? <ChatLoading /> : <div></div>}
+          {renameLoading && <ChatLoading />}
         </div>
-
         <div className={styles.nameUpdate}>
           <div className={styles.text}>Add Users</div>
           <div className={styles.nameField}>
             <input
               type="text"
+              value={search}
               onChange={handleInputChange}
               placeholder="Search users"
             />
-          </div>
-          <div className={styles.updateBtn}>
-            {/* <button onClick={() => handleSearch()}>Search</button> */}
           </div>
         </div>
         <div className={styles.addUsers}>
@@ -264,16 +236,14 @@ const UpdateGroupChatModal = ({
           ) : searchResult.length > 0 ? (
             searchResult.map((user) => (
               <User
-                user={user}
                 key={user._id}
+                user={user}
                 handleFunction={() => handleAddUser(user)}
               />
             ))
           ) : noUsers ? (
             "No users found"
-          ) : (
-            ""
-          )}
+          ) : null}
         </div>
       </div>
     </div>
