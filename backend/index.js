e5a68d694c3d8a1ba63f7116a9ecc244d81c567c
Akash290104 <mishra.ak801@gmail.com>
2024-08-27 15:22:42 +0530

    abcd

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://talk-a-tivezoneee-78p3xt94d-akashs-projects-6f1d4f45.vercel.app" // Correct origin without trailing slash
+}));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -28,7 +30,7 @@ connectDB()
     const io = new Server(server, {
       pingTimeout: 60000,
       cors: {
-        origin: "https://talk-a-tivezoneee-78p3xt94d-akashs-projects-6f1d4f45.vercel.app/", // Ensure this matches your frontend host
+        origin: "https://talk-a-tivezoneee-78p3xt94d-akashs-projects-6f1d4f45.vercel.app", // Ensure this matches your frontend host
       },
     });
 
