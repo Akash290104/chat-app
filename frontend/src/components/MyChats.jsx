b2baf106352d9140d91db35b9d03daf332ec2682
Akash290104 <mishra.ak801@gmail.com>
2024-08-27 14:37:17 +0530

    updated delay to fetch user

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -44,6 +44,10 @@ const MyChats = ({ fetchAgain }) => {
 
   useEffect(() => {
     const fetchChats = async () => {
+      while (!user || !user.data || !user.data.token) {
+        await new Promise((resolve) => setTimeout(resolve, 50)); // Polling with a small delay
+      }
+      
       if (!user || !user.data || !user.data.token) {
         console.log("User not logged in or token missing");
         return;
